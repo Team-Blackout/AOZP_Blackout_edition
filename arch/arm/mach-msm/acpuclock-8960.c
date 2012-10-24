037491907394f8a5aa52f36a5f7cddc20dabae3c
zarboz <zarboz@gmail.com>
2012-10-24 15:48:59 -0600

    adding sweep2wake. New governors and sweep2wake support

--- arch/arm/mach-msm/acpuclock-8960.c
+++ arch/arm/mach-msm/acpuclock-8960.c
@@ -608,8 +608,8 @@ static struct acpu_level acpu_freq_tbl_8960_kraitv2_slow[] = {
 	{ 1, {  1809000, HFPLL, 1, 0, 0x3E }, L2(19), 1325000 },
 	{ 1, {  1890000, HFPLL, 1, 0, 0x40 }, L2(19), 1350000 },
 #ifndef CONFIG_MSM_CPU_MAX_CLK_1DOT89GHZ
-	{ 1, {  1998000, HFPLL, 1, 0, 0x42 }, L2(19), 1350000 },
-	{ 1, {  2106000, HFPLL, 1, 0, 0x44 }, L2(19), 1350000 },
+	{ 1, {  1998000, HFPLL, 1, 0, 0x42 }, L2(20), 1350000 },
+	{ 1, {  2106000, HFPLL, 1, 0, 0x44 }, L2(21), 1350000 },
 #endif
 #endif
 #endif
@@ -647,8 +647,8 @@ static struct acpu_level acpu_freq_tbl_8960_kraitv2_nom[] = {
 	{ 1, {  1809000, HFPLL, 1, 0, 0x3E }, L2(19), 1275000 },
 	{ 1, {  1890000, HFPLL, 1, 0, 0x40 }, L2(19), 1300000 },
 #ifndef CONFIG_MSM_CPU_MAX_CLK_1DOT89GHZ
-	{ 1, {  1998000, HFPLL, 1, 0, 0x42 }, L2(19), 1325000 },
-	{ 1, {  2106000, HFPLL, 1, 0, 0x44 }, L2(19), 1350000 },
+	{ 1, {  1998000, HFPLL, 1, 0, 0x42 }, L2(20), 1325000 },
+	{ 1, {  2106000, HFPLL, 1, 0, 0x44 }, L2(21), 1350000 },
 #endif
 #endif
 #endif
@@ -686,8 +686,8 @@ static struct acpu_level acpu_freq_tbl_8960_kraitv2_fast[] = {
 	{ 1, {  1809000, HFPLL, 1, 0, 0x3E }, L2(19), 1250000 },
 	{ 1, {  1890000, HFPLL, 1, 0, 0x40 }, L2(19), 1275000 },
 #ifndef CONFIG_MSM_CPU_MAX_CLK_1DOT89GHZ
-	{ 1, {  1998000, HFPLL, 1, 0, 0x42 }, L2(19), 1300000 },
-	{ 1, {  2106000, HFPLL, 1, 0, 0x44 }, L2(19), 1325000 },
+	{ 1, {  1998000, HFPLL, 1, 0, 0x42 }, L2(20), 1300000 },
+	{ 1, {  2106000, HFPLL, 1, 0, 0x44 }, L2(21), 1325000 },
 #endif
 #endif
 #endif
